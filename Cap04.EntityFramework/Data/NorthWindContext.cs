e6fe43e0340a1f9ae5f7dcaf2cb3d72e29ca09bd
Chien Hsiao <chienh@gmail.com>
2022-11-21 16:36:25 -0300

    aula dbfirst

---
+++ Cap04.EntityFramework/Data/NorthWindContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Cap04.EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cap04.EntityFramework.Data;
+
+public partial class NorthWindContext : DbContext
+{
+    public NorthWindContext()
+    {
+    }
+
+    public NorthWindContext(DbContextOptions<NorthWindContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS; Initial Catalog=NorthWind; trusted_connection=yes; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
